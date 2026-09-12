464614e8eba7d97efcf86f45b03d0367336245e1
SimonF4 <simonetti_francoandres@hotmail.com>
2026-09-12 19:37:45 -0300

    M - Add Operaciones

--- GUIA03/File1/EjsExtra/ejs1.py
+++ GUIA03/File1/EjsExtra/ejs1.py
@@ -13,7 +13,23 @@ while ( operacion != "+" ) or ( operacion != "-" ) or ( operacion != "*" ) or( o
         "Ingrese la operacion a realizar: suma, resta, multiplicacion y division (+, -, *, /)")
 
 
+# Tdvia no usamos switch en el curso.
+if ( operacion == "+" ):
+    resultado = num1 + num2
 
+elif ( operacion == "-" ):
+    resultado = num1 - num2
 
+elif ( operacion == "*" ):
+    resultado = num1 * num2
+
+# Si o si tuvo q haber ingresado "/" xq lo filtre con el while el ingreso. => Solo valido division por 0.
+elif ( num2 != 0 ) :
+    resultado = num1 / num2
+else:
+    print("ERROR, no se puede dividir por 0.")
+    resultado = 0
 
 print(f"El resultado de la {operacion} entre {num1} y {num2} es = {resultado}")
+
+# TESTING:
