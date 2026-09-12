6db334cff397cc9524cc4d311bc0ec70fed16f0b
SimonF4 <simonetti_francoandres@hotmail.com>
2026-09-12 20:57:50 -0300

    M - Proceso, Documentacion y Testing

--- GUIA03/File1/EjsExtra/ejs2.py
+++ GUIA03/File1/EjsExtra/ejs2.py
@@ -2,11 +2,26 @@
 
 # "dada una lista de números"
 list_numeros = list( range(0, 5, 1) )
+# Esta es una forma de crear una listas vacia:
 list_resultado = list()
 
-# "generar otra con solo los pares"
 for i in list_numeros:
     print(list_numeros[i])
+
+    # "generar otra con solo los pares"
+    if (i % 2 == 0):
+        # Para agregar 1 valor a list -> con .append(valor)
+        list_resultado.append(i)
+
     i+=1
 
 print(list_resultado)
+
+# TESTING:
+# TEST 01:
+# 0
+# 1
+# 2
+# 3
+# 4
+# [0, 2, 4]
