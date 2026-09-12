0aa9125189704497470e5e1ca788c097678e36df
SimonF4 <simonetti_francoandres@hotmail.com>
2026-09-12 20:51:32 -0300

    M - List in python testing

--- GUIA03/File1/EjsExtra/ejs2.py
+++ GUIA03/File1/EjsExtra/ejs2.py
@@ -1,2 +1,12 @@
 # 🟩 Ejercicio recomendado 2 – Filtro de lista: dada una lista de números, generar otra con solo los pares (for y comprensión de listas).
 
+# "dada una lista de números"
+list_numeros = list( range(0, 5, 1) )
+list_resultado = list()
+
+# "generar otra con solo los pares"
+for i in list_numeros:
+    print(list_numeros[i])
+    i+=1
+
+print(list_resultado)
