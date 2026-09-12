1afe65c648cd8f319ea6dc546127e9ba78daf2ef
SimonF4 <simonetti_francoandres@hotmail.com>
2026-09-12 19:16:24 -0300

    M - Add ingresos del usuario

--- GUIA03/File1/EjsExtra/ejs1.py
+++ GUIA03/File1/EjsExtra/ejs1.py
@@ -1,2 +1,12 @@
 """🟩 Ejercicio recomendado 1 – Menú de calculadora: sumar, restar, multiplicar y dividir. 
 Validar división por cero."""
+
+num1 = float(input("Ingrese el 1er numero: "))
+num2 = float(input("Ingrese el 2do numero: "))
+
+operacion = input(
+    "Ingrese la operacion a realizar: suma, resta, multiplicacion y dividision (+, -, *, /)")
+
+
+
+if ( operacion != 0 ) :
