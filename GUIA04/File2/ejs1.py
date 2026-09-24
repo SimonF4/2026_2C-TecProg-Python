7884ff3da468f0d712349d58e06b7aa7dc149771
SimonF4 <simonetti_francoandres@hotmail.com>
2026-09-23 23:34:48 -0300

    Add - Comments

--- GUIA04/File2/ejs1.py
+++ GUIA04/File2/ejs1.py
@@ -14,35 +14,56 @@ amigos = ['Pepe', 'Ana', 'Jose', 'Luis']
 
 for i in range(0, len(amigos)):
     print(amigos[i], end=" ")
+# Resultado = "Pepe Ana Jose Luis "
 
 # Otra forma: for i in range(len(amigos)):  # Te lo toma igual pq por default te toma q arranca en 0.
 
-# Resultado: Pepe Ana Jose Luis 
 print("\n************************* TESTINGS: ")
-print(" - ".join(amigos))
 
-print(amigos[1])
-print(amigos[2])
-print(amigos[3])
-print(amigos[-1])
-print(amigos[0])
+print("*** Ejemplo de hacer lo mismo pero con 'join': ***")
+print(" - ".join(amigos)) # Resultado = "Pepe - Ana - Jose - Luis"
+
+print("*** TESTs de mostrar nombres con posicionamiento directo: ***")
+print(amigos[1]) # Resultado = "Ana"
+print(amigos[2]) # Resultado = "Jose"
+print(amigos[3]) # Resultado = "Luis"
+print(amigos[-1]) # Resultado = "Luis"  -> xq [-1] esto muestra siempre el ultimo.
+print(amigos[0]) # Resultado = "Pepe"   -> [0] siempre muestra el 1er item
+
+print("*** Ejemplo de Slicing: ***")
+# Slicing: para cortar la lista. Seleccionas inicio(incluye el numero) y fin(no incluye este numero).
 print(amigos[1:3]) # rta = ['Ana', 'Jose']
-amigos.append("Jessi")
-print(amigos[4])
-amigos.remove("Pepe")
-print(amigos)
-print(amigos[::-1])
 
-# nuevaList = amigos[::-1]
+print("*** Ejemplo de agregar/append: ***")
+amigos.append("Jessi")  # Devuelve "none" => no se puede meter en un print.
+print(amigos[4]) # Resultado = Jessi
+
+print("*** Ejemplo de borrar/remove: ***")
+amigos.remove("Pepe")   # Como el metodo anterior. Tmb devuelve none.
+print(amigos) # Resultado = "['Ana', 'Jose', 'Luis', 'Jessi']"
+
+print("*** Ejemplo de como invertir una lista: ***")
+# Con esto invertis el orden de una lista.
+print(amigos[::-1])  # Resultado = "['Jessi', 'Luis', 'Jose', 'Ana']"
+
+print("****** TEST para ver si te hace una copia o te modifica la lista original: **********")
+nuevaList = amigos[::-1]
 amigos.sort()
 
-print("Lista amigos: ", amigos)
-# print("Lista nueva: ",  nuevaList)
+print("Lista amigos: ", amigos) # Resultado = "Lista amigos:  ['Ana', 'Jose', 'Luis', 'Jessi']"
+print("Lista nueva: ",  nuevaList) # Resultado = "Lista nueva:  ['Jessi', 'Luis', 'Jose', 'Ana']"
 
-# print(amigos[1200])
+# Ejemplo: de como NO acceder a la ultima posicion.
+# print(amigos[1200]) # Resultado: ERROR "IndexError: list index out of range"
 
 HOLA = "Hola Mundo"
-print( HOLA[0:1])
-print( HOLA[5:15])
-print( HOLA[5:]) # Para imprimir desde x al ultimo.
-print( HOLA[2:])
+# Como se imprime la 1er letra: "H_______"
+print( HOLA[0:1])   # Resultado = H
+
+# Como se imprime desde: "____ Mundo"
+print( HOLA[5:15])  # Resultado = "Mundo"
+print("*** Para imprimir desde x valor al ultimo de una lista: ***")
+print( HOLA[5:])    # Resultado = "Mundo"
+
+# Como se imprime desde: "____la Mundo"
+print( HOLA[2:])    # Resultado = "la Mundo"
