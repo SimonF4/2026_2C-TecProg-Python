a66ab2f3143de22b97e20d8560d71c065f57f95f
SimonF4 <simonetti_francoandres@hotmail.com>
2026-09-23 22:08:04 -0300

    Add - new file + ex2 statement

---
+++ GUIA04/File2/ejs2.py
@@ -0,0 +1,8 @@
+"""
+Ejercicio 2:
+Con la misma lista de amigos (['Pepe', 'Ana', 'Jose', 'Luis']), imprime los nombres en una sola línea, pero luego agrega un nuevo amigo llamado 'Claudio'. Vuelve a imprimir la lista actualizada.
+Ejemplo de salida:
+Los nombres de mis amigos son: Pepe Ana Jose Luis
+Vamos a agregar más amigos
+Los nombres de mis amigos ahora son: Pepe Ana Jose Luis Claudio
+"""
